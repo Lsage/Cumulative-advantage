879aef3672452d6de9631690dd55d1c546e256ac
Lsage <37064724+Lsage@users.noreply.github.com>
2024-04-22 14:41:50 +0200

    Update 1_variance_analysis_appendix_sinatra_et_al_data.R

--- 1_variance_analysis_appendix_sinatra_et_al_data.R
+++ 1_variance_analysis_appendix_sinatra_et_al_data.R
@@ -12,16 +12,26 @@ if (!requireNamespace("haven", quietly = TRUE)) {
 }
 library(haven)
 
-##############################################
-# Load the scientists data which contains one row per paper 
-# and only the authorId retrived from A. Barabasi's website
-# and the citations in the 10 years following publication date
-# retrived with openAlex (https://docs.ropensci.org/openalexR/)
-small<-read_dta("write_your_path/scientists.dta")
+your_path<-"/Users/lsage/Documents/Sociologie/Recherche/Max Weber/Cumulative advantage/Scripts/reproduction_sinatra/"
+
+
+
+###################################################################
+# Same graphs with only scientists who have 50 publications or more
+small<-read_dta(paste0(your_path,"/scientists.dta"))
+
+small<-small%>%
+  group_by(authorId)%>%
+  mutate(t=1,
+         paperNumber=cumsum(t),
+         N=n())
+
+small<-small%>%
+  filter(N>49)
 
 # Number of scientists:
 length(unique(small$authorId))
-# 22861
+# 2908
 
 # Number papers in sequential order:
 small<-small%>%
@@ -60,72 +70,78 @@ res<-small%>%
   summarise(v=var(tot,na.rm=TRUE))
 
 
-# For all k
+
 main_plot<-ggplot(res,aes(x=k,y=v))+
-  geom_line()+
-  geom_point(shape=4)+
-  geom_smooth(se=FALSE,color="blue")+
+  geom_point(color="Orchid",alpha=0.8)+
+  geom_smooth(color="red")+
   labs(
-    x = "k",
-    y = "Variance"
+    x = "n",
+    # y = expression(bold(paste("Variance(", Z[n],")")))
+    y = expression(paste("Variance(", Z[n],")"))
   ) +
+  scale_y_continuous(limits=c(0.95,1.25), expand = c(0, 0))+
+  scale_x_continuous(limits=c(0,50), expand = c(0, 0))+
   theme_bw() +
   theme(
-    axis.title = element_text(size = 14, face = "bold"),
+    # axis.title = element_text(size = 14,face = "bold"),
+    # panel.grid.major.x = element_blank(),
+    panel.grid.minor.x = element_blank(),
+    # panel.grid.major.y = element_blank(),
+    panel.grid.minor.y = element_blank()
+  )
+main_plot
+
+
+ggsave(paste0(your_path,"/c_5_scientists_with_k_sup_50_for_n_up_to_50.jpeg"),
+       plot=main_plot, device = "jpeg",
+       width = 12.5, height = 9, units = "cm")
+
+
+tot<-c()
+
+for (i in 1:20){
+  small<-small%>%
+    group_by(authorId)%>%
+    mutate(x_k_t=cumsum(logCit),
+           lag_x_k_t=lag(x_k_t),
+           full=logCit-(lag_x_k_t/(paperNumber+i)))
+  
+  small2<-small%>%
+    group_by(paperNumber)%>%
+    summarise(v=var(full))%>%
+    filter(paperNumber<=50)
+  
+  small2$c<-i
+  tot<-bind_rows(tot,small2)
+  print(i)
+}
+
+tot$c_bis<-as.factor(paste0("c=", tot$c))
+tot$c_bis <- factor(tot$c_bis, levels = c("c=1", "c=2", "c=3", "c=4", "c=5", "c=6", "c=7", "c=8",
+                                          "c=9", "c=10", "c=11", "c=12", "c=13", "c=14", "c=15",
+                                          "c=16", "c=17", "c=18", "c=19", "c=20"))
+
+g<-ggplot(tot,aes(x=paperNumber,y=v)) +
+  facet_wrap(~c_bis) +
+  geom_point(color="Orchid",alpha=0.8)+
+  geom_smooth(se=FALSE,color="red")+  scale_y_continuous(limits=c(1,1.25))+
+  labs(
+    x = "n",
+    y = expression(paste("Variance(", Z[n],")"))
+  )  +
+  theme_bw() +
+  theme(
+    axis.title = element_text(size = 14),
     panel.grid.major.x = element_blank(),
     panel.grid.minor.x = element_blank(),
     panel.grid.major.y = element_blank(),
     panel.grid.minor.y = element_blank(),
-    strip.text = element_text(color = "white", face = "bold"),
-    strip.background = element_rect(fill = "grey70")  
-  )+
-  annotate(
-    "text",
-    x = Inf, y = -Inf,   
-    hjust = 1.2, vjust = -1,  
-    label = paste0("c=",round(c,2)),
-    color = "red", size = 5, fontface = "bold"  
-  )
-
-
-# For k<=50
-sub_plot<-ggplot(res[which(res$k<=50),],aes(x=k,y=v))+
-  geom_line()+
-  geom_point(shape=4)+
-  geom_smooth(se=FALSE,color="blue")+
-  labs(
-    x = "k",
-    y = "Variance"
-  ) +
-  theme_bw() +
-  theme(
-    axis.title = element_text(size = 10, face = "bold"),
-    panel.grid.major.x = element_blank(),
-    panel.grid.minor.x = element_blank(),
-    panel.grid.major.y = element_blank(),
-    panel.grid.minor.y = element_blank(),
-    strip.text = element_text(color = "white", face = "bold"),
-    strip.background = element_rect(fill = "grey70")  
-  )+
-  annotate(
-    "text",
-    x = Inf, y = -Inf,   # Position in the bottom right corner
-    hjust = 1.2, vjust = -1,  # To adjust horizontal and vertical justification
-    label = paste0("c=",round(c,2)),
-    color = "red", size = 5, fontface = "bold"  
-  )
-
-# Add the small graph to the big one and position it: 
-g<-main_plot +
-  coord_cartesian(xlim = c(1, 430), ylim = c(0, 6)) +
-  annotation_custom(
-    ggplotGrob(sub_plot),
-    xmin = 0, xmax = 282,  
-    ymin = 2.5, ymax = 6   
+    strip.text = element_text(color = "black",face="bold"),
+    strip.background = element_rect(fill = "seashell2")  
   )
 
 g
 
-# ggsave("write_your_path/variance_all_sample.jpeg",
-#        plot=g, device = "jpeg",
-#        width = 20, height = 15, units = "cm")
+ggsave(paste0(your_path,"/different_c_scientists_with_k_sup_50.jpeg"),
+       plot=g, device = "jpeg",
+       width = 20, height = 15, units = "cm")
